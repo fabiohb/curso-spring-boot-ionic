5507300d836e59ad388e0b5fc3107c6a481441cd
Fabio Henrique Barros <fabio-henrique.barros@serpro.gov.br>
2018-12-12 17:01:07 -0300

    Produto e associacao muitos para muitos

--- cursomc/src/main/java/com/fabiohb/cursos/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/fabiohb/cursos/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.fabiohb.cursos.cursomc;
 
+import static java.util.Arrays.asList;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +10,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fabiohb.cursos.cursomc.domain.Categoria;
+import com.fabiohb.cursos.cursomc.domain.Produto;
 import com.fabiohb.cursos.cursomc.repositories.CategoriaRepository;
+import com.fabiohb.cursos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,11 +30,21 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		categoriaRepository.saveAll(
-			Arrays.asList(
-				new Categoria(null, "Informática"),
-				new Categoria(null, "Escritório")
-			)
-		);
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		categoria1.getProdutos().addAll(asList(produto1, produto2, produto3));
+		categoria2.getProdutos().add(produto2);
+		
+		produto1.getCategorias().add(categoria1);
+		produto2.getCategorias().addAll(asList(categoria1, categoria2));
+		produto3.getCategorias().add(categoria1);
+		
+		categoriaRepository.saveAll(asList(categoria1, categoria2));
+		produtoRepository.saveAll(asList(produto1, produto2, produto3));
 	}
 }
--- cursomc/src/main/java/com/fabiohb/cursos/cursomc/domain/Produto.java
+++ cursomc/src/main/java/com/fabiohb/cursos/cursomc/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
@@ -16,7 +18,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -27,17 +29,25 @@ public class Categoria implements Serializable {
 
 	private String nome;
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	private Double preco;
 
-	public Categoria() {
+	@ManyToMany
+	@JoinTable(
+		name = "PRODUTO_CATEGORIA",
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
+
+	public Produto() {
 		super();
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		this();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 }
