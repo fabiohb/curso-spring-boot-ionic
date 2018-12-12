3c7ea819d456c782949fd99a38720e461b08dedd
Fabio Henrique Barros <fabio-henrique.barros@serpro.gov.br>
2018-12-12 16:17:19 -0300

    Criando operacao d instanciacao

--- cursomc/src/main/java/com/fabiohb/cursos/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/fabiohb/cursos/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.fabiohb.cursos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.fabiohb.cursos.cursomc.domain.Categoria;
+import com.fabiohb.cursos.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		categoriaRepository.saveAll(
+			Arrays.asList(
+				new Categoria(null, "Informática"),
+				new Categoria(null, "Escritório")
+			)
+		);
 	}
 }
