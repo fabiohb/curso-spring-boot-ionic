fdf7d67da6acc292f0bbe56b39d97732524741c3
Fabio Henrique Barros <fabio-henrique.barros@serpro.gov.br>
2018-12-14 14:24:02 -0300

    Inserindo uma nova Categoria com POST

--- cursomc-backend/src/main/java/com/fabiohb/cursos/cursomc/resources/CategoriaResource.java
+++ cursomc-backend/src/main/java/com/fabiohb/cursos/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,17 @@
 package com.fabiohb.cursos.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabiohb.cursos.cursomc.domain.Categoria;
 import com.fabiohb.cursos.cursomc.services.CategoriaService;
@@ -26,5 +30,16 @@ public class CategoriaResource {
 		Categoria categoria = service.buscar(id);
 		
 		return ResponseEntity.ok(categoria);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(Categoria categoria) {
+		Categoria categoriaInserida = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(categoriaInserida.getId())
+					.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
