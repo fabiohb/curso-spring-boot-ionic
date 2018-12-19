fe67c4702b9831fa1bbaf0b68ce14a27d9c444a0
Fabio Henrique Barros <fabio-henrique.barros@serpro.gov.br>
2018-12-19 12:59:14 -0300

    Validacao customizada: email nao repetido na insercao de cliente

--- cursomc-backend/src/main/java/com/fabiohb/cursos/cursomc/repositories/ClienteRepository.java
+++ cursomc-backend/src/main/java/com/fabiohb/cursos/cursomc/repositories/ClienteRepository.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fabiohb.cursos.cursomc.domain.Cliente;
 
 @Repository
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- cursomc-backend/src/main/java/com/fabiohb/cursos/cursomc/services/validation/ClienteInsertValidator.java
+++ cursomc-backend/src/main/java/com/fabiohb/cursos/cursomc/services/validation/ClienteInsertValidator.java
@@ -8,11 +8,17 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fabiohb.cursos.cursomc.dto.ClienteNewDTO;
+import com.fabiohb.cursos.cursomc.repositories.ClienteRepository;
 import com.fabiohb.cursos.cursomc.resources.exception.FieldMessage;
 import com.fabiohb.cursos.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repository;
 
 	@Override
 	public boolean isValid(ClienteNewDTO value, ConstraintValidatorContext context) {
@@ -23,9 +29,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				&& !BR.isValidCPF(value.getCpfOuCnpj())) {
 			errors.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
+
 		if (value.getTipo().equals(PESSOA_JURIDICA.getCod())
 				&& !BR.isValidCNPJ(value.getCpfOuCnpj())) {
 			errors.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		if (repository.findByEmail(value.getEmail()) != null) {
+			errors.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		// Inclua os testes aqui, inserindo erros na lista
