bed8d589898be8b53409e4d028581024a2ffbbf9
Fabio Henrique Barros <fabio-henrique.barros@serpro.gov.br>
2019-01-02 15:03:48 -0300

    Configuração e envio do projeto para produção

---
+++ src/main/java/com/fabiohb/cursos/cursomc/config/ProdConfig.java
@@ -0,0 +1,30 @@
+package com.fabiohb.cursos.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.fabiohb.cursos.cursomc.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws Exception {
+		if ("create".equals(strategy)) {
+			dbService.instantiateTestDatabase();
+		}
+
+		return true;
+	}
+
+}
